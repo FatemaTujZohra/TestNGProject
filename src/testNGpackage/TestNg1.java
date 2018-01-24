0b6f6414065c19344c05424e9e91aeba3d69a504
Fatema Tuj Zohra <ftzohratit@gmail.com>
2018-01-23 21:33:43 -0500

    First try in creating TestNG project

---
+++ src/testNGpackage/TestNg1.java
@@ -0,0 +1,13 @@
+package testNGpackage;
+
+import org.testng.annotations.Test;
+
+public class TestNg1 {
+	
+	@Test
+	public void display() {
+		
+		System.out.println(" Hi Fatema! Welcome to TestNG project");
+
+}
+}
